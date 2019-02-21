d19ff90b29ba60c20b00f54a6185b9a0e95c2009
aschemily <aschemily@gmail.com>
2019-02-20 21:29:51 -0500

    added like,dislike feature

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ class App extends Component {
   state = {
     topics: [],
     topicClicked: false,
+    displayOne: '',
     favorites:[],
+    favoriteIndex: 0,
   }
 
   componentDidMount(){
@@ -18,31 +20,48 @@ class App extends Component {
     .then(data => this.setState({topics: data}))
   }
 
-  clickTopic = (topicName)=>{
+  clickTopic = (topicName) =>{
     //console.log('in clickTopic event',topicName)
-    return this.state.topics.map(topic =>{
-      console.log('in clickTopic topic is',topic)
+   return this.state.topics.map(topic =>{
+     //console.log(topicFavorites)
+      //console.log('in clickTopic topic is',topic.favorites)
       if (topicName === topic.topic_name){
-        fetch("http://localhost:3000/api/v1/favorites")
-        .then(r => r.json())
-        .then(data => this.setState(
-          {favorites: data,
-          topicClicked:true}))
+        this.setState({favorites: topic.favorites, topicClicked:true})
       } else {
         return topicName
       }
+      //console.log('topicFavorites', topicFavorites)
     })
   }
 
+  handleNextFavorite = ()=>{
+    this.setState({favoriteIndex: this.state.favoriteIndex + 1})
+  }
+
+  favoriteToDisplay = () =>{
+    return this.state.favorites.slice(
+      this.state.favoriteIndex, this.state.favoriteIndex + 1
+    )
+  }
+
+  onSwipeMove = (position, event)=>{
+    console.log('position', position.x, 'event', event)
+    console.log('position', position.y, 'event', event)
+  }
+
+
+
 
   render() {
-
+    console.log('in state topics',this.state.topics)
+    console.log('in state favorites',this.state.favorites)
     return (
       <div>
       <h1>In APP</h1>
       {this.state.topicClicked ?
-        <Favorites favorites={this.state.favorites}/>
+       <Favorites favorites={this.favoriteToDisplay()} handleNextFavorite={this.handleNextFavorite} onSwipe={this.onSwipeMove}/>
       : <TopicContainer topics={this.state.topics} handleClick={this.clickTopic}/>
+
         }
       </div>
     );
--- src/container/Favorites.js
+++ src/container/Favorites.js
@@ -1,16 +1,28 @@
 import React, {Component} from 'react'
 import Topic from '../components/Topic'
+import Swipe from 'react-easy-swipe';
 import uuid from 'uuid'
 
 class Favorites extends Component {
 
+
   render(){
-    console.log('in Favorites props are',this.props)
+    // console.log('favorite state',this.state)
+   console.log('in Favorites props are',this.props)
     return(
+    
       <div>
       <h1>In Favorites</h1>
+      {this.props.favorites.map(favorite=>{
+        return <h1> {favorite.title}</h1>
+      })}
+
+
+      <button onClick={()=>this.props.handleNextFavorite()}>Dislike </button>
+      <button onClick={()=>this.props.handleNextFavorite()}>like </button>
 
       </div>
+
     )
   }
 
